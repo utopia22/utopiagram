963ddca06b0617140a1fce241acf641a8aaeea7f
sekishinya <sekishinya@example.com>
2020-05-19 22:32:34 +0900

    イイね機能未完

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-   devise_for :users
-    root 'pages#home'
+  devise_for :users,
+    controllers: { registrations: 'registrations' } # ここの行を追加
+  root 'posts#index'
 
-    get '/users/:id', to: 'users#show', as: 'user'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  resources :posts, only: %i(new create index show destroy) do
+    resources :photos, only: %i(create)
+    resources :likes, only: %i(create destroy)
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_123219) do
+ActiveRecord::Schema.define(version: 2020_05_19_125344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_photos_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "caption"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +54,8 @@ ActiveRecord::Schema.define(version: 2020_05_18_123219) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "photos", "posts"
+  add_foreign_key "posts", "users"
 end
