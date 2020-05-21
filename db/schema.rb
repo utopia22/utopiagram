424c6e0bdfd9cbc767982b18cd12833e3b54c752
sekishinya <sekishinya@example.com>
2020-05-21 16:44:58 +0900

    ユーザーネーム機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_103253) do
+ActiveRecord::Schema.define(version: 2020_05_21_070437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_103253) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "profile_photo"
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
